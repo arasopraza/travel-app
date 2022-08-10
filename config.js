49e4636d9f769c5929d72f8942acdcf279c71953
arasopraza <aras.opraza@gmail.com>
2022-08-10 20:51:53 +0700

    Init

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: process.env.PORT || 3001,
+    database: {
+      host: '127.0.0.1',
+      username: 'root',
+      password: null,
+      database: 'latihan'
+    }
+  }
